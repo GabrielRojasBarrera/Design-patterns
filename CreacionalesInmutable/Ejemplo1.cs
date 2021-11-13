cc659f8efbcc08ec163c003e8711ed8c893d99f2
unknown <rojasbarreragabriel@gmail.com>
2021-11-12 18:33:40 -0600

    Project

---
+++ CreacionalesInmutable/Ejemplo1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_patterns.CreacionalesInmutable
+{
+    public class Punto
+    {
+        private int x;
+        private int y;
+
+        public   Punto(int _x, int _y)
+        {
+            this.x = _x;
+            this.y = _y;
+
+
+        }
+       
+        public int geyx()
+        {
+            return x;
+        }
+        public int geyy()
+        {
+            return y;
+        }
+
+        public  Punto Offset  (int xoff, int yoff)
+        {
+            return new Punto (x + xoff, y + yoff);
+            
+        }
+    }
+}
